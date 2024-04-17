bc43078cf0d6c4d0e5a238e927fa80b90d385388
mattmartinez45 <114887556+mattmartinez45@users.noreply.github.com>
2024-04-17 00:45:52 +0000

    more edits done

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -1,6 +1,20 @@
 class Routing_controller < ApplicationController
   def dynamic
+    @num_dice = params.fetch("number_of_dice").to_i
 
+    @sides = params.fetch("how_many_sides").to_i
+
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+
+      render({:template => "game_templates/dynamic_move"})
+  end
+
+  erb(:flexible)
   end
 
   def home
